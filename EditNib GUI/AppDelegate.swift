133f16e1196d35c6359bc5eccf42ff483af80015
BrianWong <brianhuang11031@gmail.com>
2020-02-24 17:15:22 +0800

    Initial Commit

---
+++ EditNib GUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EditNib GUI
+//
+//  Created by 黃浩源 on 2020/02/24.
+//  Copyright © 2020 黃浩源. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EditNib GUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EditNib GUI
+//
+//  Created by 黃浩源 on 2020/02/24.
+//  Copyright © 2020 黃浩源. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
